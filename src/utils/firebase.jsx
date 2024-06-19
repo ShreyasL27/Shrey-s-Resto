e0285d88b15e0fa733ab038b3871d8fc29504372
ShreyasL27 <124705597+ShreyasL27@users.noreply.github.com>
2024-06-20 00:52:19 +0530

    Hosted on Firebase

---
+++ src/utils/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCvbG767pruNvlNp9C9mBAktAv0w7wfYAQ",
+  authDomain: "shreys-resto.firebaseapp.com",
+  projectId: "shreys-resto",
+  storageBucket: "shreys-resto.appspot.com",
+  messagingSenderId: "654091345983",
+  appId: "1:654091345983:web:bca9e86bc935d990e3188a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
